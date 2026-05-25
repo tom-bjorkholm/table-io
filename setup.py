bc053fbe94679f5cef70dd2f84e6374dd5b19e01
Tom Björkholm <klausuler_linnet0q@icloud.com>
2026-05-25 20:53:33 +0200

    0.9 RC

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
   name='tableio',
-  version='0.8.1',
+  version='0.9',
   description='Uniform way to write table data to and read from different ' \
     'file formats',
   author='Tom Björkholm',
